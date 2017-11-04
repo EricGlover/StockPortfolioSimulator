ddee8e17dcdc48c229103b55843996878856e4df
Eric Glover <etgt28@mail.missouri.edu>
2017-11-03 19:06:31 -0500

    readded jade for error screen

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const apiRouter = require("./routes/api");
 const app = express();
 
 // view engine setup
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "jade");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
